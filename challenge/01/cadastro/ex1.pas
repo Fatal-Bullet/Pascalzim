6622e9f2130739749a507f145fddb72080decbfe
kirito <65079453+Kirigaya-Kazuton@users.noreply.github.com>
2021-10-10 12:54:47 -0300

    Add

---
+++ challenge/01/cadastro/ex1.pas
@@ -0,0 +1,16 @@
+program ex1;
+
+var nome,Endereco:string;
+Idade: Integer;
+
+begin
+	Writeln('Cadastro de cliente');
+	write('Nome: ');
+	read(nome);
+	write('Idade: ');
+	read(idade);
+	write('Endereço: ');
+	read(endereco);
+	clrscr;
+	write('cadastro efetuado com sucesso!');
+end.
